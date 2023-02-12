2eb0aea2b971cdbeaba099a89bbca00d2e5feecb
Braidenp <braidenp17@gmail.com>
2023-02-12 11:31:06 -0700

    Added custom exception handling and validation

--- restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,12 +1,18 @@
 package com.rest.webservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class UserResource {
@@ -26,12 +32,32 @@ public class UserResource {
 	//GET /users/{id}
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id){
-		return service.findOne(id);
+		User user = service.findOne(id);
+		
+		if(user == null) 
+			throw new UserNotFoundException("id:"+id);
+		
+		return user;
 	}
 	
 	//Post /users
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
-		service.save(user);
+	public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+		User savedUser = service.save(user);
+		
+		//gets current URI and appends the id from saved user
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(savedUser.getId())
+				.toUri();
+		
+		//location - /users/{savedUser.Id}
+		return ResponseEntity.created(location).build();
+	}
+	
+	//delete /user/{id}
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id){
+		service.deleteById(id);
 	}
 }
